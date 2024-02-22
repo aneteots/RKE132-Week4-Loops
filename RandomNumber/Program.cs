cdef9e86c6c698288ef543e89a904d15d8604c87
Anete Ots <anete.ots@tktk.ee>
2024-02-22 11:41:29 +0200

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Random rnd  = new Random();
+
+int randomSum = 0;
+int myRandomNum = rnd.Next(0, 11);
+Console.WriteLine($"My random number is: {myRandomNum}");
+
+for (int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11);
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"random sum total: {randomSum}");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System.ComponentModel.Design;
+
+Random rnd  = new Random();
+int cpuRandom;
+
+bool loopActive  = true;
+
+while(loopActive)
+{
+    cpuRandom = rnd.Next(1,4);
+    Console.WriteLine("Make a guess, enter number between 1-3");
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom);
+    {
+        Console.WriteLine("Congratulations, you won!");
+        loopActive = false;
+    }
+}
+Console.WriteLine("Have a nice day!");
